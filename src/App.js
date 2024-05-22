69f0078574306a6127fc202e89da9267ad975630
kumuditha-bandara <Kumuditharajapaksha@gmail.com>
2024-05-22 16:48:06 +0530

    changes added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState,useEffect,useContext} from 'react';
+import { BrowserRouter as Router, Route, Routes,Navigate} from 'react-router-dom';
+import Navbar from "./Components/Navbar";
+import ItemManagement from './Components/ItemManagerment';
+import CategoryManagement from './Components/CategoryManagement';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import StockManagement from './Components/StockManagement';
+// import CartManagement from './Components/CartManagement';
+import { CartProvider } from './Components/CartContext' ;// Import the CartProvider
+import CartTotal from './Components/CartTotal';
+import Checkout from './Components/Checkout';
+import Dashboard from './Components/Dashboard';
+import { AuthProvider } from './Authentication/AuthProvider';
+// import ProtectedRoute from './Authentication/ProtectedRoute';
+import { AuthContext } from './Authentication/AuthProvider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Import other components
+
+const App = () => {
+    const ProtectedRoute = ({ element: Element, ...rest }) => {
+        const { authToken } = useContext(AuthContext);
+        return authToken ? <Element {...rest} /> : <Navigate to="/login" replace />;
+    };
+   
+    return (
+        
+        
+        <Router>
+            <AuthProvider>
+            <CartProvider>
+            <Navbar />
+            <CartTotal />
+            <Routes>
+            <Route path="/items" element={<ProtectedRoute element={ItemManagement} />} />
+            
+            <Route path="/categories" element={<ProtectedRoute element={CategoryManagement } />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/stock" element={<ProtectedRoute element={StockManagement } />} />
+            <Route path="/checkout" element={<ProtectedRoute element={Checkout} />} />
+            <Route path="/dashboard" element={<Dashboard/> } />
+               
+                {/* Add routes for other pages */}
+
+            </Routes>
+            </CartProvider>
+            </AuthProvider>
+        </Router>
+       
+
+    );
+};
 
 export default App;
